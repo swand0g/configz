0dd5f235fe6d8c08ffb5029be5a4cbe9993a447a
swand0g <alexanderjswanson@icloud.com>
2023-02-02 21:24:20 -0500

    misc: update

--- zprofile.zsh
+++ zprofile.zsh
@@ -1,6 +1,7 @@
 # Scripts
 ## Homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
+eval "$(thefuck --alias)" 
 
 # Aliases
 ## Git 
@@ -9,10 +10,12 @@ alias gat="git status"
 alias gim="git commit -m"
 alias gup="git push"
 alias dif="git diff"
+alias d="git diff"
 
 ## Self
 alias zed="nvim ~/.zprofile"
 alias zup="source ~/.zprofile && cp ~/.zprofile ~/developer/configs/zprofile.zsh"
+alias zuprc="source ~/.zshrc && cp ~/.zshrc ~/developer/configs/zshrc.zsh"
 
 ## Editors
 alias neo="nvim"
@@ -23,22 +26,26 @@ alias l="exa -abhHl --no-user --git"
 ## Updaters
 alias bup="bat cache --build && cp ~/.config/bat/config ~/developer/configs/bat.conf && echo \"Updated bat config! 🦇\""
 
-## Functions
-function gimp() {
-  git add .
-  if [ "$1" != "" ]
-  then
-      git commit -m "$*"
-  else
-      git commit -m "misc: update"
-  fi
-  git push origin HEAD
-}
-
+## GIMP
 alias g="gimp"
+
+## Python
+alias python="python3"
+
+## Rust
+alias c="cargo"
 
 # PATH Overrides
 ## Python 3.10
 ## The original version is saved in .zprofile.pysave
 PATH="/Library/Frameworks/Python.framework/Versions/3.10/bin:${PATH}"
+export PATH
+
+## Deno
+export DENO_INSTALL="/Users/aj/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
+# Setting PATH for Python 3.11
+# The original version is saved in .zprofile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/3.11/bin:${PATH}"
 export PATH
--- zshrc.zsh
+++ zshrc.zsh
@@ -1,30 +1,26 @@
-# NVM (Node Version Manager)
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  		  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
-## This after nvm initialization!
-autoload -U add-zsh-hook
-load-nvmrc() {
-  local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc)"
-
-  if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
-
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$node_version" ]; then
-      nvm use
-    fi
-  elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
+# Functions
+function git_branch {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
 }
-add-zsh-hook chpwd load-nvmrc
-load-nvmrc
-export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"
+
+function title {
+  echo -en "\e]2;$@\a"
+}
+
+function gimp {
+  git add .
+  if [ "$1" != "" ]
+  then
+      git commit -m "$*"
+  else
+      git commit -m "misc: update"
+  fi
+  git push origin HEAD
+}
+
+function fi {
+  fzf --preview 'cat {}'
+}
 
 # Mcfly (Great Scott!)
 eval "$(mcfly init zsh)"
@@ -32,5 +28,12 @@ eval "$(mcfly init zsh)"
 # Zoxide (Better CD)
 eval "$(zoxide init zsh)"
 
+# FNM (Fast Node Manager)
+eval "$(fnm env --use-on-cd)"
+
 # FZF (Fuzzy Finder)
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# Prompt
+setopt prompt_subst
+PROMPT='%F{220}<%f %~ $(git_branch)%F{220}>%f '
